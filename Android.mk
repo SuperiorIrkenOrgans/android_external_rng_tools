bcf5dcdae712fc57109fd051bc34486d646df376
Pavel Kirpichyov <pavel.kirpichyov@gmail.com>
2013-01-04 17:57:56 +0400

    Clean Android.mk

--- Android.mk
+++ Android.mk
@@ -1,24 +1,15 @@
 LOCAL_PATH:= $(call my-dir)
 
 # ========================================================
-# vim
+# rng-tools
 # ========================================================
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES:= \
 	rngd.c stats.c rngd_entsource.c rngd_threads.c rngd_signals.c rngd_linux.c fips.c util.c
 
-#LOCAL_C_INCLUDES += \
-#        $(LOCAL_PATH) \
-#        $(LOCAL_PATH)/proto \
-#        $(LOCAL_PATH)/auto
-
 LOCAL_SHARED_LIBRARIES += \
         libcutils 
-
-#LOCAL_CFLAGS += \
-#        -DHAVE_CONFIG_H \
-#        -lncurses
 
 LOCAL_MODULE := rngd
 LOCAL_MODULE_TAGS := optional
